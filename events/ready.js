051bac18dba57b37c85bed783385240196702ba1
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-01 16:25:25 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,14 @@
 const Discord = require("discord.js")
 
 exports.run = async(Bot) => {
-  var TotalMembers = 0
-
-  Bot.guilds.cache.forEach(guild => {
-    TotalMembers = TotalMembers + guild.memberCount
-  })
-
   Bot.user.setPresence({
     activity: {
-      name: `${process.env.prefix}Help | ${TotalMembers} users!`,
+      name: `${process.env.prefix}Help | ${Bot.TotalMembers} users!`,
       type: "WATCHING",
     },
     status: "online",
   }),
 
   console.log(`Versions: \nNode Version: ${process.version}\nDiscord Version: ${Discord.version}`)
-  console.log(`${Bot.user.tag} is now ready to come online! \nThere are currently ${Bot.guilds.cache.size} servers with ${TotalMembers} members in them.`)
+  console.log(`${Bot.user.tag} is now ready to come online! \nThere are currently ${Bot.guilds.cache.size} servers with ${Bot.TotalMembers} members in them.`)
 }
