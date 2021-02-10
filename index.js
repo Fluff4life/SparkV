a5e1b497ef6f83c3818c88342bb70ef0a3021bd0
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-09 19:57:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { ShardingManager } = require("discord.js")
 const ShardManager = new ShardingManager("./bot.js", { token: process.env.token })
 
 ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
