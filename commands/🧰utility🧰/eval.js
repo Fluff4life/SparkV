6306756b53b2f41320d5fdf9568a6d9f42fa5928
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-19 18:01:03 -0500

    Fixed

--- commands/🧰utility🧰/eval.js
+++ commands/🧰utility🧰/eval.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (Bot, msg) => {
-    if (!Bot.CheckPerms(msg)){
+    if (!Bot.CheckPerm(msg)){
         return msg.channel.send("❌Access denied.")
     }
 
---
+++ commands/🧰utility🧰/shutdown.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js");
+
+exports.run = async (Bot, msg) => {
+  if (!Bot.CheckPerm(msg)){
+    return  msg.channel.send("❌Access denied.")
+  }
+
+  const VerificationEmbed = new MessageEmbed()
+  .setTitle("Convermination Prompt")
+  .setDescription("Are you sure you want to do this?")
+  .setFooter("Canceling in 60 seconds if no emoji reacted.")
+  .setColor("#0099ff")
+
+  const VerificationMessage = await message.channel.send(VerificationEmbed)
+    const Emoji = await Bot.PromptMessage(VerificationMessage, message.author, ["✅", "❌"], 30)
+
+    if (Emoji === "✅"){
+      // Yes
+      VerificationMessage.delete()
+
+      Bot.setActivity("Bot is now going offline.", { type: "OFFLINE" })
+
+        message.channel.send({
+          embed: {
+            title: `Shutting Down`,
+            description: `Bot is now going offline. Goodnight!`,
+            color: "#0099ff",
+            
+            footer: {
+              text: "Goodnight!",
+              icon_url: process.env.bot_logo
+            }
+          },
+        })
+    } else if (emoji === "❌"){
+      VerificationMessage.delete()
+
+      message.channel.send("❌Shutdown canceled.").then(m => m.delete({ timeout: 10000 }))
+    }
+},
+  
+  exports.config = {
+    enabled: true,
+    guild_only: true,
+    aliases: ["invite", "support"],
+    bot_permissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL"]
+  },
+  
+  exports.help = {
+    name: "Invite",
+    description:
+      "Need help with me or you want to invite me to your server? This command makes inviting easy!",
+    usage: "",
+    category: "🧰utility🧰",
+    cooldown: 1.5
+  }
