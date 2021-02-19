ab99baf890bdcf1a9c35490e310e06a8929ee9c0
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-18 21:41:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ process.on("unhandledRejection", err => {
     console.log(`Unhandled rejection error! ${err}`)
 })
 
-// Start Website //
-
 // Shard Handlers //
 ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
 ShardManager.spawn()
