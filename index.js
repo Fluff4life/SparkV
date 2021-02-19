6f4568996a0812fcf3d43e4ec144f84b5d08543c
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-18 21:37:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const { ShardingManager } = require("discord.js")
 const ShardManager = new ShardingManager("./bot.js", { token: process.env.token })
 
-// Shard Handlers //
-ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
-ShardManager.spawn()
-
 // Error Handlers //
 process.on("uncaughtException", err => {
     const ErrorMessage = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./")
@@ -18,3 +14,9 @@ process.on("uncaughtException", err => {
 process.on("unhandledRejection", err => {
     console.log(`Unhandled rejection error! ${err}`)
 })
+
+// Start Website //
+
+// Shard Handlers //
+ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
+ShardManager.spawn()
