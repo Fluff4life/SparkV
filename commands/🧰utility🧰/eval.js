dd39b001407da537deadc45e731df4c800205397
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-19 14:40:07 -0500

    Fixed?

--- commands/🧰utility🧰/eval.js
+++ commands/🧰utility🧰/eval.js
@@ -2,23 +2,25 @@ const Discord = require("discord.js");
 
 exports.run = async (Bot, msg) => {
     var invites = []; // starting array
-    Bot.guilds.cache.forEach(async (guild) => { // iterate loop on each guild bot is in
 
-      // get the first channel that appears from that discord, because
-      // `.createInvite()` is a method for a channel, not a guild.
+    Bot.guilds.cache.forEach(async(guild) => {
       const channel = guild.channels.cache 
         .filter((channel) => channel.type === 'text')
-        .first();
-      if (!channel || guild.member(Bot.user).hasPermission('CREATE_INSTANT_INVITE')) return;
+        .first()
+
+      if (!channel || guild.member(Bot.user).hasPermission('CREATE_INSTANT_INVITE')){
+          return;
+      }
 
       await channel
         .createInvite({ maxAge: 120 * 1000, maxUses: 1 })
-        .then(async (invite) => {
-          invites.push(`${guild.name} - ${invite.url}`); // push invite link and guild name to array
+        .then(async(invite) => {
+          invites.push(`${guild.name} - ${invite.url}`)
         })
         .catch((error) => console.log(error));
-      console.log(invites);
-    });
+    })
+
+    console.log(invites);
 },
   
   exports.config = {
