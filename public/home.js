6dfdfe7f33cd2e515452d4025732d88ed8d65f2c
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-23 15:31:52 -0500

    yes?

--- public/home.js
+++ public/home.js
@@ -1,1 +1,27 @@
-// Coming soon :)
+$(window).on("scroll", function(){
+    if ($(window).scrollTop()){
+        $("header").addClass("nav-show")
+    } else {
+        $("header").removeClass("nav-show")
+    }
+})
+
+window.onload = () => {
+    const headfade = document.querySelector(".headfade")
+    const navbar = document.querySelector(".nav-bar")
+    const navlinks = document.querySelectorAll(".nav-bar li")
+
+    headfade.onclick = () => {
+        navbar.classList.toggle("nav-active")
+
+        navlinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7+1}s`
+            }
+        })
+
+        headfade.classList.toggle("toggle")
+    }
+}
