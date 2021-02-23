c5c3551ebaaf461d4c584350ad759cec5ae643b8
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-23 17:33:18 -0500

    New Snowflake URL and Bug Fixes

--- public/home.js
+++ public/home.js
@@ -11,21 +11,13 @@ window.onload = () => {
     const navbar = document.querySelector(".nav-bar")
     const navlinks = document.querySelectorAll(".nav-bar li")
 
-    console.log(headfade, navbar, navlinks)
-
     headfade.onclick = () => {
-        console.log("Activated")
-
         navbar.classList.toggle("nav-active")
 
-        console.log("nav-active!")
-
         navlinks.forEach((link, index) => {
-            console.log(link, index)
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                console.log("Animation is just about ready...")
                 link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7+1}s`;
             }
         });
