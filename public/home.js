419fa9278dc3e08a2e1b9363354b56d9ebfa3e39
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-24 11:34:07 -0500

    Fixed??

--- public/home.js
+++ public/home.js
@@ -1,3 +1,29 @@
+if ($(window).scrollTop()){
+  $("header").addClass("nav-show")
+} else {
+  $("header").removeClass("nav-show")
+}
+
+const onload = () => {
+  const headfade = document.querySelector(".headfade")
+  const navbar = document.querySelector(".nav-bar")
+  const navlinks = document.querySelectorAll(".nav-bar li")
+
+  headfade.onclick = () => {
+    navbar.classList.toggle("nav-active")
+
+    navlinks.forEach((link, index) => {
+      if (link.style.animation){
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7+1}s`;
+    }
+  });
+
+  headfade.classList.toggle("toggle")
+  }
+}
+
 $(document).ready = () => {
   $(document).ready(function(){
     $("a").on("click", function(event){
@@ -16,28 +42,4 @@ $(document).ready = () => {
   })
 }
 
-$(window).onload = () => {
-  const headfade = document.querySelector(".headfade")
-  const navbar = document.querySelector(".nav-bar")
-  const navlinks = document.querySelectorAll(".nav-bar li")
-
-  if ($(window).scrollTop()){
-    $("header").addClass("nav-show")
-  } else {
-    $("header").removeClass("nav-show")
-  }
-
-  headfade.onclick = () => {
-    navbar.classList.toggle("nav-active")
-
-    navlinks.forEach((link, index) => {
-      if (link.style.animation){
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7+1}s`;
-    }
-  })
-
-  headfade.classList.toggle("toggle")
-  }
-}
+$(window).onload = () => onload();
