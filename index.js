276dc440c3adbda860a31a287ccfc56c8dbc6baf
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-02-25 20:03:09 -0500

    Fixed bug

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { ShardingManager } = require("discord.js")
 
-const ShardManager = new ShardingManager("./bot.js", {
-    token: process.env.token
-})
-    
-// Shard Handlers //
-ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
-ShardManager.spawn()
+try {
+    const ShardManager = new ShardingManager("./bot.js", {
+        token: process.env.token
+    })
+
+    // Shard Handlers //
+    ShardManager.on("shardCreate", Shard => console.log(`Launched shard: ${Shard.id}.`))
+    ShardManager.spawn()
+} catch(err){
+    console.log("WARNING => Failed to activate Shard Manager. Calling bot file without sharding fetures!")
+
+    return require("./bot")
+}
