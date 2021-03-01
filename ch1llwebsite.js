6198d762a2919fa81fbf6531fe0d6020fa2a1464
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-01 15:57:15 -0500

    Debug Enabled

--- ch1llwebsite.js
+++ ch1llwebsite.js
@@ -7,7 +7,7 @@ console.log("LOADING STARTED - WEBSITE => Now loading website.")
 // Librarys //
 const express = require("express");
 
-const LocalHost = false
+// const LocalHost = false
 
 // App //
 const app = express();
@@ -15,59 +15,62 @@ app.use(express.static("public"));
 
 // Functions //
 function RunWebsite() {
+  console.log("Running...")
   if (process.env.Down === "true") {
+    console.log("Offline")
     app.use((req, res, next) => {
       res.status(500);
       res.sendFile(__dirname + `/public/html/down.html`);
     });
-
-    return
-  }
-
-  app.get("/home", (request, response) => {
-    response.sendFile(__dirname + `/public/html/home.html`);
-  });
-
-  app.get("/ch1llstudios", (request, response) => {
-    response.sendFile(__dirname + `/public/html/cshome.html`);
-  });
-
-  app.get("/ch1llblox", (request, response) => {
-    response.sendFile(__dirname + `/public/html/ch1llblox.html`);
-  });
-
-  app.get("/", (request, response) => {
-    response.redirect("/home");
-  });
-
-  app.get("/api/status", (request, response) => {
-    response.status(200).send({ status: 200, message: "OK" });
-  });
-
-  if (process.env.BotOnline == "true") {
-    app.get("/api/ch1llblox/status", (request, response) => {
+  } else {
+    console.log("E")
+    app.get("/home", (request, response) => {
+      response.sendFile(__dirname + `/public/html/home.html`);
+    });
+  
+    app.get("/ch1llstudios", (request, response) => {
+      response.sendFile(__dirname + `/public/html/cshome.html`);
+    });
+  
+    app.get("/ch1llblox", (request, response) => {
+      response.sendFile(__dirname + `/public/html/ch1llblox.html`);
+    });
+  
+    app.get("/", (request, response) => {
+      response.redirect("/home");
+    });
+  
+    app.get("/api/status", (request, response) => {
       response.status(200).send({ status: 200, message: "OK" });
     });
-  } else if (process.env.BotOnline == "false") {
-    app.get("/api/ch1llblox/status", (request, response) => {
-      response
-        .status(503)
-        .send({ status: 503, message: "down for maintenance" });
-    });
-  } else {
-    console.log("WARNING => Unknown error gettings status of Ch1llBlox.");
 
-    app.get("/api/ch1llblox/status", (request, response) => {
-      response
-        .status(500)
-        .send({ status: 500, message: "service unavailable" });
+    console.log("Hasn't crashed.")
+  
+    if (process.env.BotOnline == "true") {
+      app.get("/api/ch1llblox/status", (request, response) => {
+        response.status(200).send({ status: 200, message: "OK" });
+      });
+    } else if (process.env.BotOnline == "false") {
+      app.get("/api/ch1llblox/status", (request, response) => {
+        response
+          .status(503)
+          .send({ status: 503, message: "down for maintenance" });
+      });
+    } else {
+      console.log("WARNING => Unknown error gettings status of Ch1llBlox.");
+  
+      app.get("/api/ch1llblox/status", (request, response) => {
+        response
+          .status(500)
+          .send({ status: 500, message: "service unavailable" });
+      });
+    }
+  
+    app.use((req, res, next) => {
+      res.status(404);
+      res.sendFile(__dirname + `/public/html/404.html`);
     });
   }
-
-  app.use((req, res, next) => {
-    res.status(404);
-    res.sendFile(__dirname + `/public/html/404.html`);
-  });
 }
 
 // Code //
