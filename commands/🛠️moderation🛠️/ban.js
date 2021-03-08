d2d71a69de66b1549bfd6dda158eef4e10590c16
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-07 20:12:55 -0500

    Fixed

--- commands/🛠️moderation🛠️/ban.js
+++ commands/🛠️moderation🛠️/ban.js
@@ -4,43 +4,43 @@ exports.run = async (Bot, msg, Arguments) => {
   const UserToBan = msg.mentions.members.first() || msg.guild.members.cache.get(Arguments[0]) || msg.guild.members.cache.find(User => User.user.username.toLowerCase() === Arguments.slice(0).join(" ") || User.user.username === Arguments[0])
   const ReasonForBan = Arguments.join(" ").slice(22) || "No reason provided."
 
-  if (!Arguments[0]){
+  if (!Arguments[0]) {
     return msg.channel.send("❌Please mention someone to ban!").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (!UserToBan){
+  if (!UserToBan) {
     return msg.channel.send("❌I cannot find that member!").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (UserToBan.id === msg.author.id){
+  if (UserToBan.id === msg.author.id) {
     return msg.channel.send("❌You cannot ban yourself.").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (!UserToBan.bannable){
-      return msg.channel.send("❌Uh oh... I can't ban this user!").then(m => m.delete({ timeout: 5000 }))
+  if (!UserToBan.bannable) {
+    return msg.channel.send("❌Uh oh... I can't ban this user!").then(m => m.delete({ timeout: 5000 }))
   }
 
   const VerificationEmbed = new MessageEmbed()
-  .setTitle("Convermination Prompt")
-  .setDescription("Are you sure you want to do this?")
-  .setFooter("Canceling in 60 seconds if no emoji reacted.")
+    .setTitle("Convermination Prompt")
+    .setDescription("Are you sure you want to do this?")
+    .setFooter("Canceling in 60 seconds if no emoji reacted.")
 
-  await msg.channel.send(VerificationEmbed).then(async msg => {
-    const Emoji = await Bot.PromptMessage(msg, msg.author, ["✅", "❌"], 60)
+  const VerificationMessage = await msg.channel.send(VerificationEmbed)
+  const Emoji = await Bot.PromptMessage(VerificationMessage, msg.author, ["✅", "❌"], 60)
 
-    if (Emoji === "✅"){
-      // Yes
-      msg.delete()
+  if (Emoji === "✅") {
+    // Yes
+    msg.delete()
 
-      UserToBan.send(`You have been banned from ${guild.name}. Reason: ${ReasonForBan}.`).catch((err) => {
-        msg.channel.send(`Failed to ban. Error: ${err}`)
-      })
+    UserToBan.send(`You have been banned from ${guild.name}. Reason: ${ReasonForBan}.`).catch((err) => {
+      msg.channel.send(`Failed to ban. Error: ${err}`)
+    })
 
-      UserToBan.ban({
-        reason: ReasonForBan
-      })
+    UserToBan.ban({
+      reason: ReasonForBan
+    })
 
-      const BanEmbed = new MessageEmbed()
+    const BanEmbed = new MessageEmbed()
       .setTitle("Ban Command")
       .setDescription(`*✅Successfully Banned <@${UserToBan.id}>(${UserToBan.id})✅*`)
       .setThumbnail(msg.author.displayAvatarURL)
@@ -51,22 +51,21 @@ exports.run = async (Bot, msg, Arguments) => {
       .setTimestamp()
 
     msg.channel.send(BanEmbed);
-    } else if (emoji === "❌"){
-      msg.delete()
+  } else if (emoji === "❌") {
+    msg.delete()
 
-      msg.channel.send("❌Ban canceled.").then(m => m.delete({ timeout: 10000 }))
-    }
-  })  
+    msg.channel.send("❌Ban canceled.").then(m => m.delete({ timeout: 10000 }))
+  } 
 },
-  
-  exports.config = {
-    name: "ban",
-    description: "Is a user bothering you and keep coming back after you kick them? Using this command, they won't come back unless they are unbanned.",
-    aliases: ["pban"],
-    usage: "<user> <optional reason>",
-    category: "🛠️moderation🛠️",
-    bot_permissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL", "MANAGE_MESSAGES", "BAN_MEMBERS"],
-    member_permissions: ["BAN_MEMBERS"],
-    enabled: true,
-    cooldown: 5
-  }
+
+exports.config = {
+  name: "ban",
+  description: "Is a user bothering you and keep coming back after you kick them? Using this command, they won't come back unless they are unbanned.",
+  aliases: ["pban"],
+  usage: "<user> <optional reason>",
+  category: "🛠️moderation🛠️",
+  bot_permissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL", "MANAGE_MESSAGES", "BAN_MEMBERS"],
+  member_permissions: ["BAN_MEMBERS"],
+  enabled: true,
+  cooldown: 5
+}
--- commands/🛠️moderation🛠️/kick.js
+++ commands/🛠️moderation🛠️/kick.js
@@ -1,45 +1,45 @@
-const { MessageEmbed  } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 exports.run = async (Bot, msg, Arguments) => {
   const UserToKick = msg.guild.member(msg.mentions.users.first()) || msg.guild.members.cache.get(Arguments[0]) || `@<${Arguments[0]}>`;
   const ReasonForKick = Arguments.join(" ").slice(22) || "No reason provided."
 
 
-  if (!Arguments[0]){
+  if (!Arguments[0]) {
     return msg.channel.send("❌Please mention someone to kick!").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (!UserToKick){
+  if (!UserToKick) {
     return msg.channel.send("❌I cannot find that member!").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (UserToKick.id === msg.author.id){
+  if (UserToKick.id === msg.author.id) {
     return msg.channel.send("❌You cannot kick yourself.").then(m => m.delete({ timeout: 5000 }))
   }
 
-  if (!UserToKick.kickable){
-      return msg.channel.send("❌Uh oh... I can't kick this user!").then(m => m.delete({ timeout: 5000 }))
+  if (!UserToKick.kickable) {
+    return msg.channel.send("❌Uh oh... I can't kick this user!").then(m => m.delete({ timeout: 5000 }))
   }
 
   const VerificationEmbed = new MessageEmbed()
-  .setTitle("Convermination Prompt")
-  .setDescription("Are you sure you want to do this?")
-  .setFooter("Canceling in 60 seconds if no emoji reacted.")
+    .setTitle("Convermination Prompt")
+    .setDescription("Are you sure you want to do this?")
+    .setFooter("Canceling in 60 seconds if no emoji reacted.")
 
-  await msg.channel.send(VerificationEmbed).then(async msg => {
-    const Emoji = await Bot.PromptMessage(msg, msg.author, ["✅", "❌"], 60)
+  const VerificationMessage = await msg.channel.send(VerificationEmbed)
+  const Emoji = await Bot.PromptMessage(VerificationMessage, msg.author, ["✅", "❌"], 60)
 
-    if (Emoji === "✅"){
-      // Yes
-      msg.delete()
+  if (Emoji === "✅") {
+    // Yes
+    msg.delete()
 
-      UserToKick.send(`You have been kicked from ${guild.name}. Reason: ${ReasonForKick}.`).catch((err) => {
-        msg.channel.send(`Failed to kick. Error: ${err}`)
-      })
+    UserToKick.kick()
 
-      UserToKick.kick()
+    UserToKick.send(`You have been kicked from ${guild.name}. Reason: ${ReasonForKick}.`).catch((err) => {
+      msg.channel.send(`Failed to kick. Error: ${err}`)
+    })
 
-      const KickEmbend = new Discord.MessageEmbed()
+    const KickEmbend = new Discord.MessageEmbed()
       .setTitle("Kick Command")
       .setDescription(`*✅Successfully kicked <@${UserToKick.id}>(${UserToKick.id})✅*`)
       .setThumbnail(UserToKick.avatar)
@@ -50,22 +50,21 @@ exports.run = async (Bot, msg, Arguments) => {
       .setTimestamp();
 
     msg.channel.send(KickEmbend);
-    } else if (emoji === "❌"){
-      msg.delete()
+  } else if (emoji === "❌") {
+    msg.delete()
 
-      msg.channel.send("❌Kick canceled.").then(m => m.delete({ timeout: 10000 }))
-    }
-  })   
+    msg.channel.send("❌Kick canceled.").then(m => m.delete({ timeout: 10000 }))
+  }
 },
 
-  exports.config = {
-    name: "Kick",
-    description: "Is a user bothering you? Using this command, you can kick them from the server!",
-    aliases: [],
-    usage: "<user> <optional user>",
-    category: "🛠️moderation🛠️",
-    bot_permissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL", "MANAGE_MESSAGES", "KICK_MEMBERS"],
-    member_permissions: ["KICK_MEMBERS"],
-    enabled: true,
-    cooldown: 5
-  }
+exports.config = {
+  name: "Kick",
+  description: "Is a user bothering you? Using this command, you can kick them from the server!",
+  aliases: [],
+  usage: "<user> <optional user>",
+  category: "🛠️moderation🛠️",
+  bot_permissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL", "MANAGE_MESSAGES", "KICK_MEMBERS"],
+  member_permissions: ["KICK_MEMBERS"],
+  enabled: true,
+  cooldown: 5
+}
