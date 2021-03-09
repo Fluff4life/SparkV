b59b7b9d4859ae0d9b38eac8ef439333277cd5b4
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-09 12:22:36 -0500

    Fixed

--- ch1llwebsite.js
+++ ch1llwebsite.js
@@ -15,11 +15,11 @@ const btoa = require("btoa")
 const app = express();
 app.use(express.static("public"));
 app.use(Session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
-app.use(BodyParser.urlencoded({extended : true}));
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
 
 // Functions //
@@ -30,40 +30,22 @@ async function RunWebsite() {
       res.sendFile(__dirname + `/public/html/down.html`);
     });
   } else {
+    app.use("/discordapi", require("./public/discordapi"))
+
     app.get("/", (request, response) => {
-      response.status(200).sendFile(__dirname + `/public/html/login.html`)
-    });
-
-    app.get("/login", (request, response) => {
-      response.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=763126208149585961&scope=identify&response_type=code&redirect_uri=https://ch1ll.herokuapp.com/login/callback`)
-    });
-
-    app.get("/login/callback", async (request, response) => {
-      if (!request.query.code){
-        return response.status(404)
+      if (request.session.loggedin) {
+        response.redirect("/home")
+      } else {
+        response.status(401).sendFile(__dirname + `/public/html/login.html`);
       }
-
-      const code = request.query.code
-      const creds = btoa(`763126208149585961:${process.env.CLIENT_SECRET}`)
-      const FetchResponse = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=https://ch1ll.herokuapp.com/login/callback`, {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${creds}`
-        }
-      })
-    
-      const json = await FetchResponse.json()
-      response.redirect(`/?token=${json.access_token}`)
-    })
+    });
 
     app.get("/home", (request, response) => {
-      if (request.session.loggedin){
-        response.sendFile(__dirname + `/public/html/home.html`);
+      if (request.session.loggedin) {
+        response.status(200).sendFile(__dirname + `/public/html/home.html`);
       } else {
-        response.sendFile(__dirname + `/public/html/login.html`);
+        response.status(401).sendFile(__dirname + `/public/html/login.html`);
       }
-
-      response.end()
     });
 
     app.get("/ch1llstudios", (request, response) => {
@@ -82,21 +64,12 @@ async function RunWebsite() {
       response.status.send(404).send({ status: 404, message: "Error displaying this page: This page is under construction. Please come back soon!" })
     });
 
-    if (process.env.BotOnline === "true") {
-      app.get("/api/ch1llblox/status", (request, response) => {
-        response.status(200).send({ status: 200, message: "OK" });
-      });
-    } else {
-      app.get("/api/ch1llblox/status", (request, response) => {
-        response
-          .status(503)
-          .send({ status: 503, message: "down for maintenance" });
-      });
-    }
+    app.get("/api/ch1llblox/status", (request, response) => {
+      response.status(process.env.BotOnline).send({ status: process.env.BotOnline });
+    });
 
     app.use((req, res, next) => {
-      res.status(404);
-      res.sendFile(__dirname + `/public/html/404.html`);
+      res.status(404).sendFile(__dirname + `/public/html/404.html`);
     });
   }
 }
@@ -106,7 +79,7 @@ console.log("-------- Loading Website --------");
 RunWebsite();
 
 // Listener //
-if (process.env.LocalHost === "true"){
+if (process.env.LocalHost === "true") {
   const listener = app.listen(process.env.PORT, process.env.hostname, () => {
     console.log(`SUCCESS - WEBSITE => Server running at http://${process.env.hostname}:${listener.address().port} & listening on port ${listener.address().port}.`);
   })
