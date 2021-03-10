85e22103176d86cc26ab746e936a1407275fdce6
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-10 12:51:06 -0500

    Fixed

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -23,19 +23,6 @@ exports.run = async (Bot, message) => {
     }
   }
 
-  if(!message.partial) {
-    const channel = client.channels.cache.get('ID HERE');
-    if(channel) {
-        const embed = new MessageEmbed()
-            .setTitle('Deleted Message')
-            .addField('Author', `${message.author.tag} (${message.author.id})`, true)
-            .addField('Channel', `${message.channel.name} (${message.channel.id})`, true)
-            .setDescription(message.content)
-            .setTimestamp();
-        channel.send(embed);
-    }
-}
-
   const Logging = await Bot.Database.get(`ServerData_${message.guild.id}.Logging.enabled`)
   const LoggingChannelID = await Bot.Database.get(`ServerData_${message.guild.id}.Logging.channelID`)
 
