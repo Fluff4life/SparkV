21cfbd8ea467041f7f311f3712d18066de8bfc85
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-11 15:49:59 -0500

    No more auth required

--- website/routes/main.js
+++ website/routes/main.js
@@ -2,19 +2,19 @@ const Express = require("express")
 const CheckAuth = require("../CheckAuth")
 const Router = Express.Router()
 
-Router.get("/", CheckAuth, async (request, response) => {
+Router.get("/", async (request, response) => {
     response.redirect("/home")
 })
 
-Router.get("/home", CheckAuth, async (request, response) => {
+Router.get("/home", async (request, response) => {
     response.sendFile(__dirname + `/public/html/home.html`);
 })
 
-Router.get("/ch1llstudios", CheckAuth, async (request, response) => {
+Router.get("/ch1llstudios", async (request, response) => {
     response.sendFile(__dirname + `/public/html/cshome.html`);
 })
 
-Router.get("/ch1llblox", CheckAuth, async (request, response) => {
+Router.get("/ch1llblox", async (request, response) => {
     response.sendFile(__dirname + `/public/html/ch1llblox.html`);
 })
 
