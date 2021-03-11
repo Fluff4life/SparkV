f08f5208146bff7b4de5b105db6ad839f69bdeae
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-11 16:28:52 -0500

    Fixed

--- website/routes/api.js
+++ website/routes/api.js
@@ -2,11 +2,11 @@ const Express = require("express")
 const CheckAuth = require("../CheckAuth")
 const Router = Express.Router()
 
-Router.get("/api/status", async (request, response) => {
+Router.get("/status", async (request, response) => {
     response.status(200).send({ status: 200, message: "OK" });
 })
 
-Router.get("/api/ch1llblox/status", async (request, response) => {
+Router.get("/ch1llblox/status", async (request, response) => {
     response.status(process.env.BotOnline).send({ status: process.env.BotOnline });
 })
 
--- website/routes/main.js
+++ website/routes/main.js
@@ -7,15 +7,15 @@ Router.get("/", async (request, response) => {
 })
 
 Router.get("/home", async (request, response) => {
-    response.sendFile(`website/views/home.html`);
+    response.sendFile(`app/website/views/home.html`);
 })
 
 Router.get("/ch1llstudios", async (request, response) => {
-    response.sendFile(__dirname + `website/views/cshome.html`);
+    response.sendFile(__dirname + `app/website/views/cshome.html`);
 })
 
 Router.get("/ch1llblox", async (request, response) => {
-    response.sendFile(__dirname + `website/views/ch1llblox.html`);
+    response.sendFile(__dirname + `app/website/views/ch1llblox.html`);
 })
 
 module.exports = Router
