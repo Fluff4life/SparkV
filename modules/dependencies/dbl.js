f68b821aa7042348a26731174ca675e17f26d5a8
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-12 10:37:10 -0500

    Fixed?

--- modules/dependencies/dbl.js
+++ modules/dependencies/dbl.js
@@ -13,13 +13,4 @@ module.exports = async (Bot) => {
             shardCount: Bot.options.shardCount
         })
     }, 300 * 1000)
-
-    const Router = Express.Router()
-    const Webhook = new Topgg.Webhook("Ch1llBloxTopAuth0808")
-
-    Router.post("/ch1llblox/uservote", Webhook.middleware(), (request, response) => {
-        // User voted
-        // Request.vote.user: Gives User ID
-        console.log(`User voted! ID: ${request.vote.user}`)
-    })
 }
--- public/routes/api.js
+++ public/routes/api.js
@@ -1,5 +1,8 @@
 const Express = require("express")
+const Topgg = require("@top-gg/sdk")
+
 const Router = Express.Router()
+const Webhook = new Topgg.Webhook("Ch1llBloxTopAuth0808")
 
 Router.get("/status", async (request, response) => {
     response.status(200).send({ status: 200, message: "OK" });
@@ -7,6 +10,12 @@ Router.get("/status", async (request, response) => {
 
 Router.get("/ch1llblox/status", async (request, response) => {
     response.status(process.env.BotOnline).send({ status: process.env.BotOnline });
+})
+
+Router.post("/ch1llblox/uservote", Webhook.middleware(), (request, response) => {
+    // User voted
+    // Request.vote.user: Gives User ID
+    console.log(`User voted! ID: ${request.vote.user}`)
 })
 
 module.exports = Router
