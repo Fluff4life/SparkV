58b4e7d70cc6621ac3bbeb01efb13f45fd383714
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-15 15:38:38 -0400

    Better Sharding

--- index.js
+++ index.js
@@ -37,13 +37,33 @@ if (Config.Debug){
     const ShardManager = new Discord.ShardingManager("./ch1llblox.js", {
         token: process.env.token,
         totalShards: Number(process.env.TotalShards) || "auto",
-        // shardArgs: typeof v8debug === "object" ? ["--inspect"] : undefined,
-        // execArgv: ["--trace-warnings"]
+        shardArgs: typeof v8debug === "object" ? ["--inspect"] : undefined,
+        execArgv: ["--trace-warnings"]
     })
 
     // Shard Handlers //
     ShardManager.on("shardCreate", (Shard) => {
-        console.log(require("chalk").blue(`SUCCESS - SHARD ${Shard.id + 1}/${ShardManager.totalShards} DEPLOYED`))
+        console.log(require("chalk").green(`DEPLOYING - SHARD ${Shard.id}/${ShardManager.totalShards} DEPLOYING`))
+
+        Shard.on("ready", () => {
+            console.log(require("chalk").blue(`DEPLOY SUCCESS - SHARD ${Shard.id}/${ShardManager.totalShards} DEPLOYED SUCCESSFULLY`))
+        })
+
+        Shard.on("disconnect", (event) => {
+            console.log(require("chalk").red(`SHARD DISCONNECTED - SHARD ${Shard.id}/${ShardManager.totalShards} DISCONNECTED. ${event}`))
+        })
+
+        Shard.on("reconnecting", () => {
+            console.log(require("chalk").red(`SHARD RECONNECTING - SHARD ${Shard.id}/${ShardManager.totalShards} RECONNECTING`))
+        })
+
+        Shard.on("death", (event) => {
+            console.log(require("chalk").red(`SHARD CLOSED - SHARD ${Shard.id}/${ShardManager.totalShards} UNEXPECTEDLY CLOSED!\nPID: ${event.pid}\nExit Code: ${event.exitCode}.`))
+
+            if (!event.exitCode){
+                console.warn(`WARNING: SHARD ${Shard.id}/${ShardManager.totalShards} EXITED DUE TO LACK OF AVAILABLE MEMORY.`)
+            }
+        })
     })
 
     ShardManager.spawn(Number(process.env.TotalShards) || "auto", 8000, -1);
