dcb368146ab09f39a89dc735dd26f213c36a7953
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-03-31 16:59:59 -0400

    Reversed Update

--- ch1llwebsite.js
+++ ch1llwebsite.js
@@ -125,11 +125,11 @@ RunWebsite();
 
 // Listener //
 if (Config.Debug === true) {
-  const listener = app.listen(parseInt(process.env.port), "localhost", () => {
+  const listener = app.listen(process.env.port, "localhost", () => {
     console.log(Chalk.blue(`SUCCESS - WEBSITE => Server running at http://localhost:${listener.address().port} & listening on port ${listener.address().port}.`));
   })
 } else {
-  const listener = app.listen(parseInt(process.env.port), () => {
+  const listener = app.listen(process.env.port, () => {
     console.log(Chalk.blue(`SUCCESS - WEBSITE => Server listening on port ${listener.address().port}.`));
   })
 }
