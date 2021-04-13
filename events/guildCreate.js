8e906ec2d158bded1cec4769feace04f70583209
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-04-13 11:22:14 -0400

    🔼 Improvements!

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,7 +7,7 @@ exports.run = async(Bot, guild) => {
 
   if (guild.systemChannel && guild.systemChannel.permissionsFor(Bot.user).has("SEND_MESSAGES") && guild.systemChannel.permissionsFor(Bot.user).has("VIEW_CHANNEL")){
     try {
-      await guild.systemChannel.send("Hi! My name's Ch1llBlox. I'm a *ch1ll* bot with over 60+ commands and more are constantly being added! Simply use the command ^Help to get a list of my commands. Want to enable a setting? Do ^Help Config. See you around, new friends!")
+      await guild.systemChannel.send("Hi! My name's Ch1llBlox.I'm a bot with over 100+ commands and more constantly being added! Simply use the command ^Help to get a list of my commands. Want to enable a setting? Go to my dashboard! [Click here](https://ch1llblox.botdash.pro/). See you around, new friends!")
     } catch {
       console.log(`Failed to send message to ${guild.name} (${guild.id})!`)
     }
--- events/ready.js
+++ events/ready.js
@@ -40,29 +40,29 @@ exports.run = async (Bot) => {
         url: Activity.url
       },
     })
+
+    for (const guild of Bot.guilds.cache) {
+      if (process.env.UserBlacklist.includes(guild.ownerID)) {
+        try {
+          await guild.leave()
+  
+          console.log(`Left guild ${guild.name} because it's on the UserBlacklist.`)
+        } catch {
+          console.log(`Failed to leave Blacklisted User's Guild! GuildName: ${guild.name} GuildID: ${id}`)
+        }
+      }
+  
+      if (process.env.GuildBlacklist.includes(guild.id)) {
+        try {
+          await guild.leave()
+  
+          console.log(`Left guild ${guild.name} because it's on the GuildBlacklist.`)
+        } catch {
+          console.log(`Failed to leave Blacklisted guild! GuildName: ${guild.name} GuildID: ${id}`)
+        }
+      }
+    }
   }, 60 * 1000)
-
-  for (const guild of Bot.guilds.cache) {
-    if (process.env.UserBlacklist.includes(guild.ownerID)) {
-      try {
-        await guild.leave()
-
-        console.log(`Left guild ${guild.name} because it's on the UserBlacklist.`)
-      } catch {
-        console.log(`Failed to leave Blacklisted User's Guild! GuildName: ${guild.name} GuildID: ${id}`)
-      }
-    }
-
-    if (process.env.GuildBlacklist.includes(guild.id)) {
-      try {
-        await guild.leave()
-
-        console.log(`Left guild ${guild.name} because it's on the GuildBlacklist.`)
-      } catch {
-        console.log(`Failed to leave Blacklisted guild! GuildName: ${guild.name} GuildID: ${id}`)
-      }
-    }
-  }
 
   Bot.dashboard = new botdash.APIclient(process.env.dashapikey)
   Bot.Log("SUCCESS", Bot.user.tag, `Bot is now up and running!\nServers: ${Bot.FormatNumber(await Bot.GetServerCount())}\nUsers: ${Bot.FormatNumber(await Bot.GetUserCount())}`)
