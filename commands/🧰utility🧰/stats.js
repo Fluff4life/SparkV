eb236b702d6334ec1cfdd3ff80f38e0c749ca36f
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-04-14 16:29:03 -0400

    Added MB

--- commands/🧰utility🧰/stats.js
+++ commands/🧰utility🧰/stats.js
@@ -10,7 +10,7 @@ exports.run = async (Bot, message) => {
   const StatsEmbed = new Discord.MessageEmbed()
     .setTitle("📊 Stats 📊")
     .addField("**LATENCY**", `\`\`\`Ch1llBlox: ${Bot.ws.ping}ms\nAPI: ${BotMessage.createdAt - message.createdAt}ms\`\`\``, true)
-    .addField("**STORAGE**", `\`\`\`Memory: ${(UsedMemory / Math.pow(1024, 3)).toFixed(2)}/${(TotalMemory / Math.pow(1024, 3)).toFixed(2)} (${MemoryPersentage})\nRAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB\`\`\``, true)
+    .addField("**STORAGE**", `\`\`\`Memory: ${(UsedMemory / Math.pow(1024, 3)).toFixed(2)}/${(TotalMemory / Math.pow(1024, 3)).toFixed(2)} (${MemoryPersentage}) MB\nRAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB\`\`\``, true)
     .addField("**DATA**", `\`\`\`Uptime: ${Bot.MSToTime(Bot.uptime)}\`\`\``, true)
     .setFooter(`Ch1llBlox's Stats • ${Bot.Config.Embed.EmbedFooter}`)
     .setColor(Bot.Config.Embed.EmbedColor)
