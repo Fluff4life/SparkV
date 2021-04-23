80bd1f45f4979c2a562fb697a3a3920bc62e59ed
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-04-22 21:13:18 -0400

    Update 4.4.1 - Fixed Minor Error

--- modules/dependencies/noblox.js
+++ modules/dependencies/noblox.js
@@ -1,16 +1,14 @@
 const noblox = require("noblox.js")
 
 module.exports = async (Bot) => {
-    async function RefreshCookie(){
-        const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
-        const NewCookie = await noblox.refreshCookie(CurrentCookie)
-
-        Bot.Database.set("BotConfig.RobloxCookie", NewCookie)
-    }
-
     const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
     
     await noblox.setCookie(CurrentCookie).then(() => {
-        setInterval(RefreshCookie(), 60 * 1000)
+        setInterval(() => {
+            const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
+            const NewCookie = await noblox.refreshCookie(CurrentCookie)
+    
+            Bot.Database.set("BotConfig.RobloxCookie", NewCookie)
+        }, 60 * 1000)
     })
 }
