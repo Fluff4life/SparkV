e96348712c4a061eab95f49db826cdaa57ea8558
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-04-22 21:16:06 -0400

    Ah!

--- modules/dependencies/noblox.js
+++ modules/dependencies/noblox.js
@@ -3,7 +3,7 @@ const noblox = require("noblox.js")
 module.exports = async (Bot) => {
     const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
     
-    await noblox.setCookie(CurrentCookie).then(() => {
+    await noblox.setCookie(CurrentCookie).then(async () => {
         setInterval(() => {
             const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
             const NewCookie = await noblox.refreshCookie(CurrentCookie)
