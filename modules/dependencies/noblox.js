98b1a2c115b567fdb0d408788cda0e3315431333
King Ch1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-05-31 10:08:42 -0400

    🐛 | No Cookie Bug Fix

--- modules/dependencies/noblox.js
+++ modules/dependencies/noblox.js
@@ -1,18 +1,5 @@
 const noblox = require("noblox.js")
 
 module.exports = async (Bot) => {
-    if (process.env.alpha === "true"){
-        return
-    }
-    
-    const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
-    
-    await noblox.setCookie(CurrentCookie).then(async () => {
-        setInterval(async () => {
-            const CurrentCookie = await Bot.Database.get("BotConfig.RobloxCookie")
-            const NewCookie = await noblox.refreshCookie(CurrentCookie)
-    
-            Bot.Database.set("BotConfig.RobloxCookie", NewCookie)
-        }, 60 * 1000)
-    })
+    await noblox.setCookie(process.env.RobloxCookie)
 }
