8e2ff78ff80c5c5783339121b4ed1f05f8bf7b16
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-06-16 18:26:12 -0400

    ✨ | Improved Credits.js

---
+++ bot/commands/🧰utility🧰/credits.js
@@ -0,0 +1,37 @@
+const Discord = require(`discord.js`);
+
+const Credits = [
+  {
+    name: "**🛠 | Developers**",
+    value: "The people who made Ch1llBlox!\n**KingCh1ll** - Head Developer\n**Qu1ckly_Frost** - Developer",
+    inline: true
+  },
+  {
+    name: "**✨ | Contributors**",
+    value: "People that have contributed to Ch1llBlox.\n**2Lost4Discord** - Getting the bot verified.",
+    inline: true
+  }
+]
+
+exports.run = async (Bot, message) => {
+    const NewEmbed = new Discord.MessageEmbed()
+      .setTitle("Credits")
+      .setDescription(`Here's the list of people who've helped Ch1llBlox on his path to success!`)
+      .setColor(Bot.Config.Bot.Embed.Color)
+      .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+      .addFields(Credits)
+
+    return message.lineReplyNoMention(NewEmbed)
+},
+
+  exports.config = {
+    name: `Credits`,
+    description: `Look at everyone who helped make Ch1llBlox!`,
+    aliases: [],
+    usage: ``,
+    category: `🧰utility🧰`,
+    bot_permissions: [`SEND_MESSAGES`, `EMBED_LINKS`, `VIEW_CHANNEL`],
+    member_permissions: [],
+    enabled: true,
+    cooldown: 1.5
+  }
