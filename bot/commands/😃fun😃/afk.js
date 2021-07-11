85a26d4e4deb8c8f4236081f523a4ca9ada20d0a
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-07-11 08:48:48 -0400

    ✨ | AFK Command Debug

--- bot/commands/😃fun😃/afk.js
+++ bot/commands/😃fun😃/afk.js
@@ -3,33 +3,49 @@ const Discord = require(`discord.js`);
 const afk = require("../../../models/afk")
 
 exports.run = async (Bot, message, Arguments) => {
-  if (message.mentions.roles.first()) {
-    return message.channel.send("I cannot ping roles.")
-  }
+  console.log("AFK COMMAND WORKING")
 
   const data = await afk.findOne({
     UserID: message.author.id
   })
 
+  console.log("AFK COMMAND WORKING 2")
+
   if (!data) {
+    console.log("AFK COMMAND NO DATA")
+
     const reason = args.slice(0).join(" ") || "No reason supplied."
 
+
+    console.log(reason)
     try {
       const newAfk = new afk({
         UserID: message.author.id,
         Reason: reason
       })
 
+      console.log("New AFK")
+
       newAfk.save()
+
+      console.log("Saved")
 
       message.lineReply(`You're now AFK. Reason: ${reason}`)
     } catch (err) {
+      console.error(err)
+
       message.lineReply("Failed to save AFK status.")
     }
   } else {
-    await afk.deleteOne({
-      UserID: message.author.id
-    })
+    try {
+      await afk.deleteOne({
+        UserID: message.author.id
+      })
+    } catch (err) {
+      console.error(err)
+
+      message.lineReply("Failed to delete AFK status.")
+    }
 
     message.lineReply(Bot.Config.Bot.Responses.AFKWelcomeMessage)
   }
