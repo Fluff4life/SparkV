a114cb4431e39441a83d804bf74b9ac6a2ae443d
KingCh1ll <KingCh1ll@users.noreply.github.com>
2021-09-05 03:38:13 +0000

    🎨 | Cleaned Up Code

--- modules/updateDocs.js
+++ modules/updateDocs.js
@@ -7,48 +7,54 @@ module.exports = {
    * @param {Object} bot The Ch1llBlox bot Instance.
    */
   update(bot) {
-	let cmdCount = 0;
+    let cmdCount = 0;
 
-	bot.commands.forEach(() => ++cmdCount);
+    bot.commands.forEach(() => ++cmdCount);
 
-	let baseText = `# Commands\nCh1llBlox's Command List! Ch1llBlox contains more than **${Math.floor(cmdCount)} commands**!\n\n`;
+    let baseText = `# Commands\nCh1llBlox's Command List! Ch1llBlox contains more than **${Math.floor(
+      cmdCount,
+    )} commands**!\n\n`;
 
-	bot.categories.sort((a, b) => {
-		const aCmds = bot.commands.filter(c => c.category === a).array();
-		const bCmds = bot.commands.filter(c => c.category === b).array();
+    bot.categories
+      .sort((a, b) => {
+        const aCmds = bot.commands.filter(c => c.category === a).array();
+        const bCmds = bot.commands.filter(c => c.category === b).array();
 
-		if (aCmds.length > bCmds.length) {
-			return -1;
-		} else {
-			return 1;
-		}
-	}).forEach(cat => {
-		const info = ["Name", "Description", "Usage", "Cooldown"];
-		const cmds = bot.commands.filter(cmd => cmd.category === cat).array();
+        if (aCmds.length > bCmds.length) {
+          return -1;
+        } else {
+          return 1;
+        }
+      })
+      .forEach(cat => {
+        const info = ["Name", "Description", "Usage", "Cooldown"];
+        const cmds = bot.commands.filter(cmd => cmd.category === cat).array();
 
-		baseText += `### ${cat}\n\n`;
+        baseText += `### ${cat}\n\n`;
 
-		cmds.sort((a, b) => {
-			if (a.name < b.name) {
-				return -1;
-			} else {
-				return 1;
-			}
-		}).forEach(cmd => {
-			info.push([
-				`**${cmd.name || "Command name invalid."}**`,
-				cmd.description || "No description for this command",
-				cmd.usage || "",
-				`${Math.ceil(cmd.cooldown / 1000)} seconds`
-			]);
-		});
+        cmds
+          .sort((a, b) => {
+            if (a.name < b.name) {
+              return -1;
+            } else {
+              return 1;
+            }
+          })
+          .forEach(cmd => {
+            info.push([
+              `**${cmd.name || "Command name invalid."}**`,
+              cmd.description || "No description for this command",
+              cmd.usage || "",
+              `${Math.ceil(cmd.cooldown / 1000)} seconds`,
+            ]);
+          });
 
-		baseText += `${mtable(info)}\n\n`;
-	});
+        baseText += `${mtable(info)}\n\n`;
+      });
 
-	if (fs.existsSync("../docs")) {
-		fs.writeFileSync("../docs/commands.md", baseText);
-		console.log("📋 | Successfully updated commands!");
-	}
+    if (fs.existsSync("../docs")) {
+      fs.writeFileSync("../docs/commands.md", baseText);
+      console.log("📋 | Successfully updated commands!");
+    }
   },
 };
