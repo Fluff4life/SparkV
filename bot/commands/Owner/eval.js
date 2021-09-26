98b0be92b7a83b32fbbd51605f10a8806ae598d1
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-09-26 12:59:54 -0400

    🚀 | Fixed Eval

--- bot/commands/Owner/eval.js
+++ bot/commands/Owner/eval.js
@@ -11,17 +11,16 @@ async function execute(bot, message, args, command, data) {
 
   let input = args.join(" ");
   let hasAsync = input.includes("return") || input.includes("await");
-  let result, evalTime;
+  let result;
 
   try {
-    const before = Date.now();
-
     result = await eval(hasAsync ? `(async()=>{${input}})();` : input);
-    evalTime = Date.now() - before;
 
     if (typeof result !== "string") {
       result = inspect(result, {
-        depth: +!(inspect(result, { depth: 1 }).length > 1000)
+        depth: +!(inspect(result, {
+          depth: 1
+        }).length > 1000)
       });
     }
 
@@ -30,16 +29,17 @@ async function execute(bot, message, args, command, data) {
     result = err.message;
   }
 
-  if (input.length + result.length > 2000) {
-    const paste = await fetch(`https://hastepaste.com/api/create?raw=false&ext=javascript&text=${encodeURIComponent(`${input}\n\n${result}`)}`).catch(err => message.reply(err.message));
+  if (input.length + result.length >= 4000) {
+    const paste = await fetch(`https://hastepaste.com/api/create?raw=false&ext=javascript&text=${encodeURIComponent(`${input}\n\n${result}`)}`, {
+      method: "POST"
+    }).catch(err => message.reply(err.message));
 
-    return message.reply(`Eval exceeds 2000 characters. Please view here: ${paste.body}`);
+    return message.reply(`Eval exceeds 4000 characters. Please view here: ${paste.body}`);
   } else {
     const Embed = new Discord.MessageEmbed()
       .setTitle(`${bot.config.Emojis.success} | Eval Results`)
       .addField(`Input`, `\`\`\`${input}}\`\`\``)
       .addField(`Output`, `\`\`\`js\n${result}\`\`\``)
-      .setFooter(`${evalTime || evalTime === 0 ? `evaluated in ${evalTime}ms` : ""}`)
       .setColor(`GREEN`);
 
     return message.reply({
