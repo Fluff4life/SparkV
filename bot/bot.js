4badbcc27d685e33e5693b7f07365ac4d4d642af
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-09-28 12:02:07 -0400

    🚀 | Added 2 Important Intents

--- bot/bot.js
+++ bot/bot.js
@@ -27,8 +27,8 @@ const SparkV = new Client({
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     Intents.FLAGS.GUILD_VOICE_STATES,
-    // Intents.FLAGS.GUILD_MEMBERS,
-    // Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_PRESENCES,
   ],
   makeCache: Options.cacheWithLimits({
     MessageManager: 200,
