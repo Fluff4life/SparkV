f43145990a04118e69b90f900b18afc1d41d8d29
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-10-01 16:42:06 -0400

    🚀 | Added a Service Worker

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,71 +1,45 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    const isLocalhost = Boolean(
-      window.location.hostname === "localhost" ||
-        window.location.hostname === "[::1]" ||
-        window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[01]?)){3}$/),
-    );
-    const swUrl = `/service-worker.js`;
+self.addEventListener("install", event => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open("offline");
 
-    if (isLocalhost) {
-      checkValidServiceWorker(swUrl);
+      await cache.add(new Request("/assets/offline.html", { cache: "reload" }));
+    })(),
+  );
 
-      navigator.serviceWorker.ready.then(() => console.log(`⚙ | Service Worker online.`));
-    } else {
-      registerValidSW(swUrl);
-    }
-  });
-}
+  self.skipWaiting();
+});
 
-function registerValidSW(swUrl) {
-  navigator.serviceWorker
-    .register(swUrl)
-    .then(registration => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    (async () => {
+      if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable();
+    })(),
+  );
 
-        if (installingWorker === null) {
-          return;
-        }
+  self.clients.claim();
+});
 
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === "installed") {
-            if (navigator.serviceWorker.controller) {
-              console.log(
-                "⚙ | Service Worker: New content is avalible and will be used when all tabs for this page are closed.",
-              );
-            }
-          } else {
-            console.log("⚙ | Service Worker: Content is ready for offline usage.");
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const PreloadResponse = await event.preloadResponse;
+
+          if (PreloadResponse) {
+            return PreloadResponse;
           }
-        };
-      };
-    })
-    .catch(err => console.error(`⛔ | Service Worker: An error occoured while registration. ${err}`));
-}
 
-function checkValidServiceWorker(swUrl) {
-  fetch(swUrl, {
-    headers: {
-      "Service-Worker": "script",
-    },
-  })
-    .then(response => {
-      const contentType = response.headers.get("content-type");
+          const NetworkResponse = await fetch(event.request);
 
-      if (response.status === 404 || (contentType !== null && contentType.indexOf("javascript") === -1)) {
-        navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
-      } else {
-        registerValidSW(swUrl);
-      }
-    })
-    .catch(err =>
-      console.log(
-        `⛔ | Failed to connect. Please check your network, or be forced to run in offline mode (only when avalible). This could be an error on our behalf. ${err}`,
-      ),
+          return NetworkResponse;
+        } catch (err) {
+          const cache = await caches.open("offline");
+
+          return await cache.match("/assets/offline.html");
+        }
+      })(),
     );
-}
+  }
+});
