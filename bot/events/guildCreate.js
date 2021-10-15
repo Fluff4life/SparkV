b92c6e21d600787a38c7241b2a615d623e711d8b
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-10-15 16:58:19 -0400

    🚀 | Updated Bot Link Top.gg

--- bot/events/guildCreate.js
+++ bot/events/guildCreate.js
@@ -46,17 +46,15 @@ module.exports = {
         .setStyle("LINK");
 
       const VoteButton = new Discord.MessageButton()
-        .setURL("https://top.gg/bot/763126208149585961")
+        .setURL("https://top.gg/bot/884525761694933073")
         .setLabel("Review/Vote for me!")
         .setStyle("LINK");
 
-      await guild.systemChannel
-        .send({
-          content:
-            "Hi! My name's SparkV. I'm a powerful multipurpose meme/chat bot with over 100+ commands to keep your server entertained and active! All, without spending a dime. Simply use the command ^Help to get a list of my commands. Want to enable a setting? Go to my dashboard! Use the command ^Dashboard and click on the link I send you. Thanks for inviting me!\n\nPsst... don't froget to review me on top.gg!",
-          components: [new Discord.MessageActionRow().addComponents(InviteButton, SupportButton, VoteButton)],
-        })
-        .catch(err => console.log(`Failed to send message to ${guild.name} (${guild.id})! ${err.message}`));
+      await guild.systemChannel.send({
+        content:
+          "Hi! My name's SparkV. I'm a powerful multipurpose meme/chat bot with over 100+ commands to keep your server entertained and active! All, without spending a dime. Simply use the command ^Help to get a list of my commands. Want to enable a setting? Go to my dashboard! Use the command ^Dashboard and click on the link I send you. Thanks for inviting me!\n\nPsst... don't froget to review me on top.gg!",
+        components: [new Discord.MessageActionRow().addComponents(InviteButton, SupportButton, VoteButton)],
+      }).catch(err => console.log(`Failed to send message to ${guild.name} (${guild.id})! ${err.message}`));
     }
   },
 };
