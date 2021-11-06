4cfdef282828667587ad9e632085cdf6b888c6ed
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2021-11-06 11:03:17 -0400

    🚀 | Fixed Translation Replies for Slash Commands

--- bot/commands/Utility/date.js
+++ bot/commands/Utility/date.js
@@ -3,11 +3,7 @@ const canvacord = require("canvacord");
 
 const cmd = require("../../templates/command");
 
-async function execute(bot, message) {
-	return await message.replyT(`The current date is <t:${~~(Date.now() / 1000)}:F>.`);
-}
-
-module.exports = new cmd(execute, {
+module.exports = new cmd(async (bot, message) => await message.replyT(`The current date is <t:${~~(Date.now() / 1000)}:F>.`), {
 	description: "Gets the current date.",
 	dirname: __dirname,
 	aliases: [],
